f85b95dbf923c87b4976ac5be8534388ea657092
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-06-27 18:32:05 +0330

    Create Custom Colors Class

---
+++ lib/util/custom_color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class CustomColor {
+  static const Color blue = Color(0xff3B5EDF);
+  static const Color white = Color(0xffEEEEEE);
+  static const Color gray = Color(0xff858585);
+  static const Color green = Color(0xff1DB68B);
+  static const Color red = Color(0xffD02026);
+  static const Color darkBlue = Color(0xff253DEE);
+  static const Color lightBlue = Color(0xff58AEE8);
+  static const Color lightBlue2 = Color(0xff3B5EDF);
+}
