28e36fdbfe85ba3621aa93eda03ab9d09819df68
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-07-11 13:40:56 +0330

    create order model & adapter for hive

---
+++ lib/data/model/order.dart
@@ -0,0 +1,33 @@
+import 'package:e_commerce_app/data/model/productvariant.dart';
+import 'package:hive/hive.dart';
+part 'order.g.dart';
+
+@HiveType(typeId: 0)
+class OrderModel {
+  @HiveField(0)
+  String? id;
+  @HiveField(1)
+  int? count;
+  @HiveField(2)
+  String? name;
+  @HiveField(3)
+  String? garanty;
+  @HiveField(4)
+  int? persent;
+  @HiveField(5)
+  int? realPrice;
+  @HiveField(6)
+  int? discountPrice;
+  @HiveField(7)
+  List<ProductVariantModel>? variants;
+  OrderModel({
+    required this.id,
+    required this.count,
+    required this.name,
+    required this.garanty,
+    required this.persent,
+    required this.realPrice,
+    required this.discountPrice,
+    required this.variants,
+  });
+}
---
+++ lib/data/model/order.g.dart
@@ -0,0 +1,62 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class OrderModelAdapter extends TypeAdapter<OrderModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  OrderModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return OrderModel(
+      id: fields[0] as String?,
+      count: fields[1] as int?,
+      name: fields[2] as String?,
+      garanty: fields[3] as String?,
+      persent: fields[4] as int?,
+      realPrice: fields[5] as int?,
+      discountPrice: fields[6] as int?,
+      variants: (fields[7] as List?)?.cast<ProductVariantModel>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, OrderModel obj) {
+    writer
+      ..writeByte(8)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.count)
+      ..writeByte(2)
+      ..write(obj.name)
+      ..writeByte(3)
+      ..write(obj.garanty)
+      ..writeByte(4)
+      ..write(obj.persent)
+      ..writeByte(5)
+      ..write(obj.realPrice)
+      ..writeByte(6)
+      ..write(obj.discountPrice)
+      ..writeByte(7)
+      ..write(obj.variants);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is OrderModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
